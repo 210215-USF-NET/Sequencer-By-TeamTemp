af80a88116a69f027aa857ff928806c332cacee8
Soulzeon <79062042+Soulzeon@users.noreply.github.com>
2021-03-19 14:07:06 -0700

    Started Models

---
+++ RevMixer/MixerModels/Track.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MixerModels
+{
+    public class Track
+    {
+        public int Id { get; set; }
+        public int ProjectId { get; set; }
+        public int SampleId { get; set; }
+        public int PatternId { get; set; }
+
+        public Pattern Pattern { get; set; }
+    }
+}
---
+++ RevMixer/MixerModels/UploadMusic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace MixerModels
+{
+    public class UploadMusic
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public bucket MusicByte { get; set; }
+        public string Name { get; set; }
+        public DateTime UploadDate { get; set; }
+        public int Likes { get; set; }
+        public int Plays { get; set; }
+
+        public User User { get; set; }
+    }
+}
