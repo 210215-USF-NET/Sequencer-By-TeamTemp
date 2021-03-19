512ce833b2cc702fa7d18658dfda2d6e6797fc31
Matt Hunt <wmatthunt@gmail.com>
2021-03-19 16:43:07 -0400

    added BL

--- RevMixer/MixerBL/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MixerBL
-{
-    public class Class1
-    {
-    }
-}
---
+++ RevMixer/MixerBL/IMixerBL.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MixerModels;
+
+namespace MixerBL
+{
+    public interface IMixerBL
+    {
+        Task<UploadedMusic> AddUploadedMusicAsync(UploadedMusic newUploadedMusic);
+        Task<UploadedMusic> DeleteUploadedMusicAsync(UploadedMusic uploadedMusic2BDeleted);
+        Task<UploadedMusic> GetUploadedMusicByNameAsync(string name);
+        Task<List<UploadedMusic>> GetUploadedMusicAsync();
+        Task<UploadedMusic> UpdateUploadedMusicAsync(UploadedMusic uploadedMusic2BUpdated);
+        Task<User> AddUserAsync(User newUser);
+        Task<User> DeleteUserAsync(UploadedMusic user2BDeleted);
+        Task<User> GetUserByNameAsync(string name);
+        Task<List<User>> GetUsersAsync();
+        Task<User> UpdateUserAsync(User user2BUpdated);
+        Task<SavedProject> AddSavedProjectAsync(SavedProject newSavedProject);
+        Task<SavedProject> DeleteSavedProjectAsync(SavedProject savedProject2BDeleted);
+        Task<SavedProject> GetSavedProjectByNameAsync(string name);
+        Task<List<SavedProject>> GetSavedProjectsAsync();
+        Task<SavedProject> UpdateSavedProjectAsync(SavedProject savedProject2BUpdated);
+        Task<Sample> AddSampleAsync(Sample newSample);
+        Task<Sample> DeleteSampleAsync(Sample sample2BDeleted);
+        Task<Sample> GetSampleByNameAsync(string name);
+        Task<List<Sample>> GetSamplesAsync();
+        Task<Sample> UpdateSampleAsync(Sample sample2BUpdated);
+         Task<Track> AddTrackAsync(Track newTrack);
+        Task<Track> DeleteTrackAsync(Track track2BDeleted);
+        Task<Track> GetTrackByNameAsync(string name);
+        Task<List<Track>> GetTrackAsync();
+        Task<Track> UpdateTrackAsync(Track track2BUpdated);
+         Task<Pattern> AddPatternAsync(Pattern newPattern);
+        Task<Pattern> DeletePatternAsync(Pattern pattern2BDeleted);
+        Task<Pattern> GetPatternByNameAsync(string name);
+        Task<List<Pattern>> GetPatternAsync();
+        Task<Pattern> UpdatePatternAsync(Pattern pattern2BUpdated);
+    }
+}
