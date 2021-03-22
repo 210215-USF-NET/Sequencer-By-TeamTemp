e20ab6a0e8dcd4697517aecab469f9d35466721e
Weston Davidson <59481331+westondavidson@users.noreply.github.com>
2021-03-22 17:35:05 -0400

    updated DL and interface to include bottom half of necessary data access methods

--- RevMixer/MixerDL/MixerRepoDB.cs
+++ RevMixer/MixerDL/MixerRepoDB.cs
@@ -4,11 +4,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace MixerDL
 {
     class MixerRepoDB : IMixerRepoDB
     {
+        private readonly MixerDBContext _context;
+
+        public MixerRepoDB(MixerDBContext context)
+        {
+            _context = context;
+        }
+
         public Task<Pattern> AddPatternAsync(Pattern newPattern)
         {
             throw new NotImplementedException();
@@ -141,106 +149,238 @@ namespace MixerDL
             throw new NotImplementedException();
         }
 
-        public Task<List<UploadMusic>> GetUploadedMusicAsync()
+        public async Task<List<UploadMusic>> GetUploadedMusicAsync()
         {
-            throw new NotImplementedException();
+            return await _context.UploadMusic
+                .Include(um => um.User)
+                .Include(um => um.Comments)
+                .ToListAsync();
         }
 
-        public Task<UploadMusic> GetUploadedMusicByNameAsync(string name)
+        public async Task<UploadMusic> GetUploadedMusicByIDAsync(int id)
         {
-            throw new NotImplementedException();
+            return await _context.UploadMusic
+                .Include(um => um.User)
+                .Include(um => um.Comments)
+                .Where(um => um.Id == id)
+                .FirstOrDefaultAsync();
         }
 
-        public Task<User> GetUserByNameAsync(string name)
+        public async Task<User> GetUserByIDAsync(int id)
         {
-            throw new NotImplementedException();
+            return await _context.User
+                .Include(u => u.PlayLists)
+                .Include(u => u.UserProjects)
+                .Include(u => u.Sample)
+                .Include(u => u.Comments)
+                .Include(u => u.UploadMusics)
+                .Where(u => u.Id == id)
+                .FirstOrDefaultAsync();
+
         }
 
-        public Task<List<User>> GetUsersAsync()
+        public async Task<List<User>> GetUsersAsync()
         {
-            throw new NotImplementedException();
+            return await _context.User
+                .Include(u => u.PlayLists)
+                .Include(u => u.UserProjects)
+                .Include(u => u.Sample)
+                .Include(u => u.Comments)
+                .Include(u => u.UploadMusics)
+                .ToListAsync();
         }
-        public Task<UserProject> GetUserProjectByNameAsync(string name)
+        public async Task<UserProject> GetUserProjectByIDAsync(int id)
         {
-            throw new NotImplementedException();
-        }
-
-        public Task<List<UserProject>> GetUserProjectsAsync()
-        {
-            throw new NotImplementedException();
-        }
-        public Task<PlayList> GetPlayListByNameAsync(string name)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<List<PlayList>> GetPlayListsAsync()
-        {
-            throw new NotImplementedException();
-        }
-        public Task<MusicPlaylist> GetMusicPlaylistByNameAsync(string name)
-        {
-            throw new NotImplementedException();
+            return await _context.UserProject
+                .Include(up => up.User)
+                .Include(up => up.SavedProject)
+                .ThenInclude(sp => sp.Tracks)
+                .ThenInclude(t => t.Sample)
+                .Include(up => up.SavedProject)
+                .ThenInclude(sp => sp.Tracks)
+                .ThenInclude(t => t.Pattern)
+                .Where(up => up.Id == id)
+                .FirstOrDefaultAsync();
         }
 
-        public Task<List<MusicPlaylist>> GetMusicPlaylistsAsync()
+        public async Task<List<UserProject>> GetUserProjectsAsync()
         {
-            throw new NotImplementedException();
+            return await _context.UserProject
+                .Include(up => up.User)
+                .Include(up => up.SavedProject)
+                .ThenInclude(sp => sp.Tracks)
+                .ThenInclude(t => t.Sample)
+                .Include(up => up.SavedProject)
+                .ThenInclude(sp => sp.Tracks)
+                .ThenInclude(t => t.Pattern)
+                .ToListAsync();
         }
-        public Task<Comments> GetCommentByNameAsync(string name)
+        public async Task<PlayList> GetPlayListByIDAsync(int id)
         {
-            throw new NotImplementedException();
-        }
-
-        public Task<List<Comments>> GetCommentsAsync()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Pattern> UpdatePatternAsync(Pattern pattern2BUpdated)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Sample> UpdateSampleAsync(Sample sample2BUpdated)
-        {
-            throw new NotImplementedException();
+            return await _context.PlayList
+                .Include(p => p.MusicPlaylist)
+                .ThenInclude(mp => mp.UploadMusic)
+                .Include(p => p.User)
+                .Where(p => p.Id == id)
+                .FirstOrDefaultAsync();
         }
 
-        public Task<SavedProject> UpdateSavedProjectAsync(SavedProject savedProject2BUpdated)
+        public async Task<List<PlayList>> GetPlayListsAsync()
         {
-            throw new NotImplementedException();
+            return await _context.PlayList
+                .Include(p => p.MusicPlaylist)
+                .ThenInclude(mp => mp.UploadMusic)
+                .Include(p => p.User)
+                .ToListAsync();
+        }
+        public async Task<MusicPlaylist> GetMusicPlaylistByIDAsync(int id)
+        {
+            return await _context.MusicPlaylist
+                .Include(mp => mp.UploadMusic)
+                .Include(mp => mp.PlayList)
+                .Where(mp => mp.Id == id)
+                .FirstOrDefaultAsync();
         }
 
-        public Task<Track> UpdateTrackAsync(Track track2BUpdated)
+        public async Task<List<MusicPlaylist>> GetMusicPlaylistsAsync()
         {
-            throw new NotImplementedException();
+            return await _context.MusicPlaylist
+                .Include(mp => mp.UploadMusic)
+                .Include(mp => mp.PlayList)
+                .ToListAsync();
+        }
+        public async Task<Comments> GetCommentByIDAsync(int id)
+        {
+            return await _context.Comments
+                .Include(c => c.User)
+                .Include(c => c.UploadedMusic)
+                .Where(c => c.Id == id)
+                .FirstOrDefaultAsync();
         }
 
-        public Task<UploadMusic> UpdateUploadedMusicAsync(UploadMusic uploadedMusic2BUpdated)
+        public async Task<List<Comments>> GetCommentsAsync()
         {
-            throw new NotImplementedException();
+            return await _context.Comments
+                .Include(c => c.User)
+                .Include(c => c.UploadedMusic)
+                .AsNoTracking()
+                .ToListAsync();
         }
 
-        public Task<User> UpdateUserAsync(User user2BUpdated)
+        public async Task<Pattern> UpdatePatternAsync(Pattern pattern2BUpdated)
         {
-            throw new NotImplementedException();
+            Pattern oldPattern = await _context.Pattern.Where(s => s.Id == pattern2BUpdated.Id).FirstOrDefaultAsync();
+
+            _context.Entry(oldPattern).CurrentValues.SetValues(pattern2BUpdated);
+
+            await _context.SaveChangesAsync();
+
+            _context.ChangeTracker.Clear();
+            return oldPattern;
         }
-        public Task<UserProject> UpdateUserProjectAsync(UserProject userProject2BUpdated)
+
+        public async Task<Sample> UpdateSampleAsync(Sample sample2BUpdated)
         {
-            throw new NotImplementedException();
+            Sample oldSample = await _context.Sample.Where(s => s.Id == sample2BUpdated.Id).FirstOrDefaultAsync();
+
+            _context.Entry(oldSample).CurrentValues.SetValues(sample2BUpdated);
+
+            await _context.SaveChangesAsync();
+
+            _context.ChangeTracker.Clear();
+            return oldSample;
         }
-        public Task<PlayList> UpdatePlayListAsync(PlayList playList2BUpdated)
+
+        public async Task<SavedProject> UpdateSavedProjectAsync(SavedProject savedProject2BUpdated)
         {
-            throw new NotImplementedException();
+            SavedProject oldSavedProject = await _context.SavedProject.Where(sp => sp.Id == savedProject2BUpdated.Id).FirstOrDefaultAsync();
+
+            _context.Entry(oldSavedProject).CurrentValues.SetValues(savedProject2BUpdated);
+
+            await _context.SaveChangesAsync();
+
+            _context.ChangeTracker.Clear();
+            return oldSavedProject;
         }
-        public Task<MusicPlaylist> UpdateMusicPlaylistAsync(MusicPlaylist musicPlaylist2BUpdated)
+
+        public async Task<Track> UpdateTrackAsync(Track track2BUpdated)
         {
-            throw new NotImplementedException();
+            Track oldTrack = await _context.Track.Where(t => t.Id == track2BUpdated.Id).FirstOrDefaultAsync();
+
+            _context.Entry(oldTrack).CurrentValues.SetValues(track2BUpdated);
+
+            await _context.SaveChangesAsync();
+
+            _context.ChangeTracker.Clear();
+            return track2BUpdated;
         }
-        public Task<Comments> UpdateCommentAsync(Comments comment2BUpdated)
+
+        public async Task<UploadMusic> UpdateUploadedMusicAsync(UploadMusic uploadedMusic2BUpdated)
         {
-            throw new NotImplementedException();
+            UploadMusic oldUploadedMusic = await _context.UploadMusic.Where(um => um.Id == uploadedMusic2BUpdated.Id).FirstOrDefaultAsync();
+
+            _context.Entry(oldUploadedMusic).CurrentValues.SetValues(uploadedMusic2BUpdated);
+
+            await _context.SaveChangesAsync();
+
+            _context.ChangeTracker.Clear();
+            return uploadedMusic2BUpdated;
+        }
+
+        public async Task<User> UpdateUserAsync(User user2BUpdated)
+        {
+            User oldPlaylist = await _context.User.Where(u => u.Id == user2BUpdated.Id).FirstOrDefaultAsync();
+
+            _context.Entry(oldPlaylist).CurrentValues.SetValues(user2BUpdated);
+
+            await _context.SaveChangesAsync();
+
+            _context.ChangeTracker.Clear();
+            return user2BUpdated;
+        }
+        public async Task<UserProject> UpdateUserProjectAsync(UserProject userProject2BUpdated)
+        {
+            UserProject oldProject = await _context.UserProject.Where(up => up.Id == userProject2BUpdated.Id).FirstOrDefaultAsync();
+
+            _context.Entry(oldProject).CurrentValues.SetValues(userProject2BUpdated);
+
+            await _context.SaveChangesAsync();
+
+            _context.ChangeTracker.Clear();
+            return userProject2BUpdated;
+        }
+        public async Task<PlayList> UpdatePlayListAsync(PlayList playList2BUpdated)
+        {
+            MusicPlaylist oldPlaylist = await _context.MusicPlaylist.Where(p => p.Id == playList2BUpdated.Id).FirstOrDefaultAsync();
+
+            _context.Entry(oldPlaylist).CurrentValues.SetValues(playList2BUpdated);
+
+            await _context.SaveChangesAsync();
+
+            _context.ChangeTracker.Clear();
+            return playList2BUpdated;
+        }
+        public async Task<MusicPlaylist> UpdateMusicPlaylistAsync(MusicPlaylist musicPlaylist2BUpdated)
+        {
+            MusicPlaylist oldMusicPlaylist = await _context.MusicPlaylist.Where(mp => mp.Id == musicPlaylist2BUpdated.Id).FirstOrDefaultAsync();
+
+            _context.Entry(oldMusicPlaylist).CurrentValues.SetValues(musicPlaylist2BUpdated);
+
+            await _context.SaveChangesAsync();
+
+            _context.ChangeTracker.Clear();
+            return musicPlaylist2BUpdated;
+        }
+        public async Task<Comments> UpdateCommentAsync(Comments comment2BUpdated)
+        {
+            Comments oldComment = await _context.Comments.Where(c => c.Id == comment2BUpdated.Id).FirstOrDefaultAsync();
+
+            _context.Entry(oldComment).CurrentValues.SetValues(comment2BUpdated);
+
+            await _context.SaveChangesAsync();
+
+            _context.ChangeTracker.Clear();
+            return comment2BUpdated;
         }
     }
 }
